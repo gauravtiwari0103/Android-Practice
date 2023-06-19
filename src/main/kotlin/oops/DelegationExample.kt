aa4c8c640d19cd2e160440f1c5382cb5c995bdbd
Gaurav Tiwari <gaurav.tiwari@raweng.com>
2023-06-19 18:01:05 +0530

    OOPS added

---
+++ src/main/kotlin/oops/DelegationExample.kt
@@ -0,0 +1,42 @@
+package oops
+
+import java.util.*
+import kotlin.reflect.KProperty
+
+/*
+we want setters and getters for both first name and last name in class student
+we want same validation logic in both setter and getter
+but its not good practice to write same code twice
+so we would create a name delegator object to for setting and getting values
+ */
+
+class Student{
+    var firstNamee: String? by NameDelegate()
+}
+
+class NameDelegate{
+    var formattedValue: String? = null
+    operator fun setValue(
+        thisRef: Any?, // has referece of the object which will have this setter,
+        // here it is Student obj, we cant use it like (thisRef as Student).firstName
+        property: KProperty<*>,
+        //Has the metadata about the object
+        value: String?
+    ){
+        //our logic
+        println(property.name)
+        if(value != null && value.length >=5){
+            formattedValue = value.trim().uppercase(Locale.getDefault())
+        }
+    }
+
+    operator fun getValue(thisRef: Any?, kProperty: KProperty<*>) : String? {
+        return formattedValue;
+    }
+}
+
+
+fun main(){
+    var s: Student= Student()
+    s.firstNamee = "Gaurav"
+}
---
+++ src/main/kotlin/oops/Lazy.kt
@@ -0,0 +1,21 @@
+package oops
+
+class StudentHeavy {
+    var name:String = "ram"
+    init {
+        println("Student heavey created")
+    }
+}
+
+class Studentt{
+    val heavy by lazy { StudentHeavy() }
+}
+
+fun main(){
+    var s: Studentt = Studentt()
+    println("lafsdj")
+    s.heavy
+    s.heavy
+}
+
+
